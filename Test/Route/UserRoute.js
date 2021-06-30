0b28f96cab3cd852d508635057a784cd1b639cd7
parasyadav <paras.19jdds014@jietjodhpur.ac.in>
2021-06-30 15:02:44 +0530

    nouse

--- Test/Route/UserRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express();
-const userController = require("../Controllers/UserControllers");
-
-router.post("/posts", userController.posts);
-
-router.get("/posts/:id/comments", userController.postid);
-
-module.exports = router;
